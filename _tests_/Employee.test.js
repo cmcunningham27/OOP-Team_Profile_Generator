92f46d9653b686d8b0c717b2923d4bafb0c8715f
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-04-08 15:31:38 -0700

    ran test in terminal and noticed only my first test worked correctly for Employee.js; combined the 2-4th shoulds into one should by declaring and giving value for name, id, email, and employee/ it worked; then i changed the Employee.js getName function because I was not checking to see if it was with a capital letter in the beginning, once I added that check I declared 2 variables, one for the input name and one for the instance of Employee with the input name as the parameter, called those variables later and it worked; did this for when the name does not have a capital latter in the beginning and it worked as well; I realized my test coding was not accurate; I do like the test functionality.

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,3 +1,4 @@
+const { expect } = require("@jest/globals");
 const Employee = require("../lib/Employee");
 
 describe ("Employee", () => {
@@ -5,39 +6,53 @@ describe ("Employee", () => {
         const employee = new Employee();
         expect(typeof employee).toBe("object");
     });
-    it("should set name for employee", () => {
-        expect(employee.name("Ben")).toBe("Ben");
+    it("should set name, id, and email for employee", () => {
+        const name = "Ben";
+        const id = 1;
+        const email = "stteps@yahoo.com";
+        const employee = new Employee(name, id, email);
+        expect(employee.name).toBe(name);
+        expect(employee.id).toBe(id);
+        expect(employee.email).toBe(email);
     });
-    it("should set id for employee", () => {
-        expect(employee.id("1")).toBe("1");
-    });
-    it("should set email for employee", () => {
-        expect(employee.email("stteps@yahoo.com")).toBe("stteps@yahoo.com");
-    })
+    // it("should set id for employee", () => {
+       
+    //     const employee = new Employee("Foo", id);
+    //     expect(employee.id).toBe(id);
+    // });
+    // it("should set email for employee", () => {
+        
+    //     const employee = new Employee(email);
+    //     expect(employee.email).toBe(email);
+    // })
     describe ("getName", () => {
         it("should return a string with the first letter capitalized", () => {
-            expect(employee.getName("Ben")).toBe("Ben");
+            const name = "Ben";
+            const employee = new Employee(name);
+            expect(employee.getName(name)).toBe("Ben");
         });
         it("input without capital returns error message", () => {
-            expect(employee.getName("ben")).toBe("Make sure first letter is capitalized");
+            const name = "ben";
+            const employee = new Employee(name);
+            expect(employee.getName(name)).toBe("Make sure first letter is capitalized");
         });
     });
     describe ("getId", () => {
         it("should return a string with a number in it", () => {
-            expect(employee.getId("1")).toBeInstanceOf(number);
+            expect(new Employee.getId("1")).toBeInstanceOf(number);
         });
         it("incorrect input returns error message", () => {
-            expect(employee.getId("a")).toBe("Make sure type in number");
+            expect(new Employee.getId("a")).toBe("Make sure type in number");
         }); 
     })
     describe("getEmail", () => {
         it("should return an email", () => {
-            expect(employee.getEmail("stteps@yahoo.com")).toBe("stteps@yahoo.com");
+            expect(new Employee.getEmail("stteps@yahoo.com")).toBe("stteps@yahoo.com");
         });
     });
     describe("getRole", () => {
         it("Should return Employee", () => {
-            expect(employee.getRole()).toBe("Employee");
+            expect(new Employee.getRole()).toBe("Employee");
         });
     });
 });
